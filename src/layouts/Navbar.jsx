c14263643591c4511fc142a31c068e2848ed1bc6
dalienst <oduordalienst@gmail.com>
2023-07-23 22:55:47 +0300

    setup navbar and footer

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { publicLinks } from "../constants/links";
+import logo from "../images/logowhite.svg";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <nav
+        className="navbar navbar-expand-md sticky-top navbar-dark"
+        style={{ backgroundColor: "#000" }}
+      >
+        <div className="container-fluid">
+          <Link to={publicLinks.Home} className="navbar-brand">
+            <img src={logo} alt="" />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapsibleNavbar"
+            aria-controls="collapsibleNavbar" // Add aria-controls attribute
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-export default Navbar
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="collapsibleNavbar"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link">Reports</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link">Account</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link">About App</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
